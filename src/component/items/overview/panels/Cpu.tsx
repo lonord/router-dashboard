e2eddce5b4fa312e92a6a1d1311d9685433ce876
lonord <lonord.b@gmail.com>
2019-03-02 15:21:32 +0800

    add cpu temp display

--- src/component/items/overview/panels/Cpu.tsx
+++ src/component/items/overview/panels/Cpu.tsx
@@ -3,6 +3,7 @@ import { Theme } from '@material-ui/core/styles/createMuiTheme'
 import Typography from '@material-ui/core/Typography'
 import { makeStyles, useTheme } from '@material-ui/styles'
 import { CSSProperties } from '@material-ui/styles/withStyles'
+import * as Color from 'color'
 import React from 'react'
 
 const processBarHeight = 280
@@ -12,14 +13,16 @@ const processBarWidthSmall = 60
 
 export interface CpuPanelProps {
 	data: {
-		usage: number[]
+		usage: number[],
+		temp: number
 	}
 }
 
 const useStyles = makeStyles((theme: Theme) => ({
 	root: {
 		display: 'flex',
-		justifyContent: 'space-around'
+		justifyContent: 'space-around',
+		marginBottom: 10
 	} as CSSProperties,
 	loading: {
 		display: 'flex',
@@ -59,6 +62,22 @@ const useStyles = makeStyles((theme: Theme) => ({
 	} as CSSProperties,
 	itemText: {
 		textAlign: 'center'
+	} as CSSProperties,
+	progressBarWrap: {
+		display: 'flex',
+		marginBottom: 10
+	} as CSSProperties,
+	progressBarText: {
+		width: 60,
+		textAlign: 'center'
+	} as CSSProperties,
+	progressBar: {
+		height: 20,
+		borderRadius: 3,
+		flexGrow: 1
+	} as CSSProperties,
+	progressBarSecondColor: {
+		backgroundColor: Color(theme.palette.secondary.light).alpha(0.4).toString()
 	} as CSSProperties
 }))
 
@@ -67,21 +86,52 @@ export default function CpuPanel({ data }: CpuPanelProps) {
 	const theme = useTheme<Theme>()
 	console.log(theme.breakpoints.down('xs'))
 	return (
-		<div className={classes.root}>
-			{data ? data.usage.map((usage, idx) => (
-				<div key={idx} className={classes.item}>
-					<Typography className={classes.itemText}>{Math.floor(usage)}%</Typography>
-					<div className={classes.itemProgressBarWrap}>
-						<LinearProgress variant="determinate"
-							className={classes.itemProgressBar}
-							value={usage} />
+		<div>
+			{data && (
+				<>
+					<Typography>Usage</Typography>
+					<div className={classes.root}>
+						{data.usage.map((usage, idx) => (
+							<div key={idx} className={classes.item}>
+								<Typography className={classes.itemText}>{Math.floor(usage)}%</Typography>
+								<div className={classes.itemProgressBarWrap}>
+									<LinearProgress variant="determinate"
+										className={classes.itemProgressBar}
+										value={usage} />
+								</div>
+								<Typography className={classes.itemText}>{`CPU#${idx}`}</Typography>
+							</div>
+						))}
 					</div>
-					<Typography className={classes.itemText}>{`CPU#${idx}`}</Typography>
-				</div>
-			)) : (
+					<Typography>Temp</Typography>
+					<div className={classes.progressBarWrap}>
+						<LinearProgress variant="determinate"
+							classes={{ root: classes.progressBar, colorSecondary: classes.progressBarSecondColor }}
+							color="secondary"
+							value={data.temp / 1000} />
+						<Typography className={classes.progressBarText}>
+							{trimDec((data.temp / 1000) + '')}°C
+						</Typography>
+					</div>
+				</>
+			)}
+			{!data && (
+				<div className={classes.root}>
 					<Typography className={classes.loading}>Loading...</Typography>
-				)
-			}
+				</div>
+			)}
 		</div>
 	)
+}
+
+function trimDec(s: string): string {
+	if (!s || s.indexOf('.') === -1) {
+		return s
+	}
+	const idx = s.indexOf('.')
+	const decCount = s.length - idx - 1
+	if (decCount <= 1) {
+		return s
+	}
+	return s.substr(0, s.length - decCount + 1)
 }
